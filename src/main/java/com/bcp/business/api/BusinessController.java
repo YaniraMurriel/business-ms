20ac566a54da9288aa3805ead9df65c44ba548fe
Leonardo Vargas <leonardo.vargas@la-mark.com>
2021-02-08 16:07:49 -0500

    add feing

--- src/main/java/com/bcp/business/api/BusinessController.java
+++ src/main/java/com/bcp/business/api/BusinessController.java
@@ -1,5 +1,7 @@
 package com.bcp.business.api;
 
+import com.bcp.business.core.service.BusinessService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,16 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/business/")
+@RequestMapping("/v1/business")
 public class BusinessController {
+
+    @Autowired
+    BusinessService businessService;
 
     @GetMapping
     public ResponseEntity businessMethod() {
-        return ResponseEntity.ok("HOLA MUNDO GET");
+        return ResponseEntity.ok(businessService.callBusinessMethod());
     }
 
-    @PostMapping
-    public ResponseEntity businessPostMethod() {
-        return ResponseEntity.ok("HOLA MUNDO POST");
-    }
 }
--- src/test/java/com/bcp/business/BusinessMsApplicationTests.java
+++ src/test/java/com/bcp/business/BusinessMsApplicationTests.java
@@ -2,10 +2,12 @@ package com.bcp.business;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootTest
 @EnableSwagger2
+@EnableFeignClients
 class BusinessMsApplicationTests {
 
     @Test
